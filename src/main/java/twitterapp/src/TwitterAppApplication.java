760510554a1040bf7c3ef44710b0b511a1f3a6c2
Josephine <jos.a.cornelius@gmail.com>
2018-07-20 10:56:18 -0500

    added logging with stack trace

--- src/main/java/twitterapp/src/TwitterAppApplication.java
+++ src/main/java/twitterapp/src/TwitterAppApplication.java
@@ -1,13 +1,19 @@
 
 package twitterapp.src;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
+import org.slf4j.LoggerFactory;
 
 
 public class TwitterAppApplication extends Application<TwitterAppConfiguration>
 {
+
     public static void  main (String[] args) throws Exception{
+
         new TwitterAppApplication().run(args);
     }
 
--- src/main/java/twitterapp/src/TwitterAppResource.java
+++ src/main/java/twitterapp/src/TwitterAppResource.java
@@ -1,8 +1,12 @@
 package twitterapp.src;
 
-
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -23,8 +27,7 @@ public class TwitterAppResource {
     Twitter t;
     List<Status> statuses;
 
-    Logger LOGGER = LoggerFactory.getLogger(TwitterAppResource.class);
-
+    Logger log = (Logger) LoggerFactory.getLogger("myLogger");
     public TwitterAppResource(Twitter twitter){
 
         t = twitter;
@@ -38,14 +41,14 @@ public class TwitterAppResource {
         try {
             statuses = t.getHomeTimeline();
             if (statuses != null) {
-                LOGGER.info("Timeline has been printed.");
+                log.info("Timeline has been printed.");
                 return Response.ok(statuses, MediaType.APPLICATION_JSON_TYPE).build();
             } else {
-                    LOGGER.error("List of Statuses are null.");
+                    log.error("List of Statuses are null.");
                     return serverError().entity("There was a problem on the server side, please try again later.").build();
             }
         } catch (TwitterException e) {
-            LOGGER.error("There was a problem on the server side.");
+            log.error("There was a problem on the server side.", e);
             return Response.serverError().entity("There was a problem on the server side, please try again later.").build();
         }
     }
@@ -55,22 +58,22 @@ public class TwitterAppResource {
     @Path("/tweet")
     public Response postTweet(String tweet){
     if (tweet.length() > MAX_LENGTH) {
-        LOGGER.error("Tweet is too long, keep it within 280 characters");
+        log.error("Tweet is too long, keep it within 280 characters");
         return Response.serverError().entity("Tweet is too long, keep it within 280 characters").build();
     }
     else if(tweet.length() == 0){
-        LOGGER.error("An empty tweet was entered.");
-        return Response.serverError().entity("An empty tweet entered").build();
+        log.error("No tweet entered");
+        return Response.serverError().entity("No tweet entered").build();
     }
     else {
         try {
             t.updateStatus(tweet);
         }
         catch (Exception e) {
-            LOGGER.error("There was a problem on the server side, please try again later.");
+            log.error("There was a problem on the server side, please try again later.", e);
             return Response.serverError().entity("There was a problem on the server side, please try again later.").build();
         }
-        LOGGER.info("Tweet("+tweet+") has been posted.");
+        log.info("Tweet("+tweet+") has been posted.");
         return Response.ok(statuses, MediaType.APPLICATION_JSON_TYPE).entity("Tweet("+tweet+") has been posted.").build();
         }
     }
