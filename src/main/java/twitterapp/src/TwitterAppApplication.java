90d2e49286440307fa585b5c99166b146ba281ee
Josephine <jos.a.cornelius@gmail.com>
2018-08-16 14:51:30 -0500

    added CORS handling, for cross origin issues

--- src/main/java/twitterapp/src/TwitterAppApplication.java
+++ src/main/java/twitterapp/src/TwitterAppApplication.java
@@ -1,5 +1,8 @@
 package twitterapp.src;
-
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 import twitterapp.src.injections.DaggerTwitterComponent;
@@ -18,6 +21,16 @@ public class TwitterAppApplication extends Application<TwitterAppConfiguration>
     @Override
     public void run(final TwitterAppConfiguration configuration, final Environment environment)
     {
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
         TwitterModule twitterModule = new TwitterModule(configuration.twitterConfig);
         TwitterComponent component = DaggerTwitterComponent.builder()
                 .twitterModule(twitterModule)
